9bba6db2ffaeb92fc15e4bcffc869d88f408d1ae
vanbungkring <prasetyo.mailbox@gmail.com>
2016-06-24 22:04:32 +0700

    Initial Commit

---
+++ Kiosk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kiosk
+//
+//  Created by Arie on 6/24/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kiosk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kiosk
+//
+//  Created by Arie on 6/24/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
